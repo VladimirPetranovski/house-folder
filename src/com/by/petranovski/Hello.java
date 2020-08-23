41c1905381e79ff156237cdf91734155aef0ff6c
VladimirPetranovski <desantluninec@mail.ru>
2020-08-23 17:15:26 +0300

    Check my changes

--- src/com/by/petranovski/Hello.java
+++ src/com/by/petranovski/Hello.java
@@ -3,5 +3,6 @@ package com.by.petranovski;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Vladimir!");
+        System.out.println("И Серый");
     }
 }
