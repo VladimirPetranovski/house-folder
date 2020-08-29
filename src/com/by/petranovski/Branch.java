7b8bac4c9e9bc880f1be7fb38dcb19c1e923ef8b
VladimirPetranovski <desantluninec@mail.ru>
2020-08-29 11:25:38 +0300

    Adding changes Branch.java

--- src/com/by/petranovski/Branch.java
+++ src/com/by/petranovski/Branch.java
@@ -1,4 +1,8 @@
 package com.by.petranovski;
 
 public class Branch {
+    public static void main(String[] args) {
+        System.out.println("d");
+        System.out.println("рылворлор = ");
+    }
 }
