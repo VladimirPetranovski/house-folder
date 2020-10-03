dfcd16c27d22dbe997af41df938d115cd4899899
VladimirPetranovski <68049638+VladimirPetranovski@users.noreply.github.com>
2020-10-03 15:34:31 +0300

    Delete Hello.java

--- src/com/by/petranovski/Hello.java
+++
@@ -1,8 +0,0 @@
-package com.by.petranovski;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello Vladimir!");
-        System.out.println("И Серый");
-    }
-}
