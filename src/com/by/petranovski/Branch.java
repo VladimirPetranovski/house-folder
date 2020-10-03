1f869b4e40fd10837edbdc59f9bb8ad93dc7d506
VladimirPetranovski <68049638+VladimirPetranovski@users.noreply.github.com>
2020-10-03 15:34:19 +0300

    Delete Branch.java

--- src/com/by/petranovski/Branch.java
+++
@@ -1,8 +0,0 @@
-package com.by.petranovski;
-
-public class Branch {
-    public static void main(String[] args) {
-        System.out.println("d");
-        System.out.println("рылворлор = ");
-    }
-}
