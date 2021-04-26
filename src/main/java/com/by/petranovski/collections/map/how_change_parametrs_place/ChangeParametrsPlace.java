4826464c92b6f031007bbd6101d4958e16a3e3c9
VladimirPetranovski <desantluninec@mail.ru>
2021-04-26 14:03:50 +0300

    Added package collections and class ChangeParametrsPlace

---
+++ src/main/java/com/by/petranovski/collections/map/how_change_parametrs_place/ChangeParametrsPlace.java
@@ -0,0 +1,35 @@
+package com.by.petranovski.collections.map.how_change_parametrs_place;
+/*
+Напишите метод, который получает на вход Map<K, V> и возвращает Map,
+где ключи и значения поменяны местами. Так как значения могут совпадать,
+то тип значения в Map будет уже не K, аCollection<K>:
+ */
+
+import java.util.*;
+
+public class ChangeParametrsPlace {
+
+    public static <K, V> Map<V, Collection<K>> inverse(Map<? extends K, ? extends V> map){
+        Map<V, Collection<K>> resultMap = new HashMap<>();
+
+        Set<K> keys = (Set<K>) map.keySet();
+        for(K key : keys){
+            V value = map.get(key);
+            resultMap.compute(value, (v, ks) -> {
+                if(ks == null){
+                    ks = new HashSet<>();
+                }
+                ks.add(key);
+                return ks;
+            });
+        }
+
+        return resultMap;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("kjlhl", 3);
+        System.out.println(inverse(map));
+    }
+}
